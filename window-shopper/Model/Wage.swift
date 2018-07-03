a2fe7d6732ff621e2fde631964968a3369764690
Raul Villarreal <raulvilla0797@gmail.com>
2018-07-03 13:42:42 -0600

    Unit Test

---
+++ window-shopper/Model/Wage.swift
@@ -0,0 +1,15 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Raul Ernesto Villarreal Sigala on 7/3/18.
+//  Copyright © 2018 Raul Ernesto Villarreal Sigala. All rights reserved.
+//
+
+import Foundation
+
+class Wage {
+    class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        return Int(ceil(price / wage))
+    }
+}
